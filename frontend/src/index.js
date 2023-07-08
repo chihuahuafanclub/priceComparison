98e9230bff827c623446b5d16acb834ddb673f60
Howard Tseng <Kk035945542@gmail.com>
2023-07-09 06:05:29 +0800

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+
+import Home from './Home';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
